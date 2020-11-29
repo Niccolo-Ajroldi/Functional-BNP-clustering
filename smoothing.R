bc036c159bddb9e05d4b32a37df1aaf0c1eb5734
tbortolotti <teresabortolotti16@gmail.com>
2020-11-29 18:54:54 +0100

    First Smoothing

--- smoothing.R
+++ smoothing.R
@@ -4,7 +4,7 @@ library(knitr)
 library(fields)
 library(compositions)
 
-setwd("C:/Users/Teresa Bortolotti/Desktop/ProgettoBayesiana")
+setwd("C:/Users/Teresa Bortolotti/Documents/R/bayes_project/Functional-BNP-clustering")
 load("data_extraction.RData")
 
 # extract only one componet per patient
@@ -21,12 +21,19 @@ for(i in 2:26){
 curves <- my_data$data
 t_ax <- as.matrix(1:1600)
 
-#There seem to be three clusters of functions: in fact, one of them is obtained by warping the abscissas.
-
 
 # Plot of original functions
 x11()
 matplot(t_ax,t(curves), type='l', xlab='x', ylab='orig.func')
+
+
+# First Approach - Spline Regression-----------------------------------------
+
+## Do spline regression (n_knots = 1600) with B-splines of order 4 and penalizing the 1st derivative
+## Does it make sense? I know that my ideal curve should have two peaks. Would it be better to penalize
+## 2nd derivative in order to have fewer "spurious" peaks?)
+
+# Penalize first derivative
 
 n <- dim(curves)[1]
 m <- 4
@@ -36,9 +43,11 @@ pen_smooth1 <- matrix(0, dim(curves)[1], dim(curves)[2])
 pen_smooth2 <- matrix(0, dim(curves)[1], dim(curves)[2])
 
 
-basis <- create.bspline.basis(t_ax, norder=m)
+basis <- create.bspline.basis(breaks = t_ax, norder=m)
 
 functionalPar <- fdPar(fdobj=basis, Lfdobj=1, lambda=1e3)
+
+# WARNING! It takes forever, go down and load "smoothed_data1e3.RData"
 
 # test on only one function
 penal <- smooth.basis(t_ax, curves[1,], functionalPar)
@@ -65,15 +74,156 @@ for (i in 1:n) {
 }
 
 
+load("smoothed_1600b_1D_1e3.RData")
+
 # compare original and smoothed data
 x11()
-par(mfrow=c(1,2))
+par(mfrow=c(2,2))
 matplot(t_ax, t(curves), type = 'l', main = 'Original Data', ylim = c(-250,250))
 matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data', ylim = c(-250,250))
-
+matplot(t_ax, t(pen_smooth1), type = 'l', main = 'Original Data - First Derivative')
+matplot(t_ax, t(pen_smooth2), type = 'l', main = 'Smoothed Data - Second Derivative')
 
 x11()
 par(mfrow=c(1,2))
 matplot(t_ax, t(curves), type = 'l', main = 'Original Data')
 matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data')
+
+
+# Penalize 2nd derivative, lambda = 1e3
+
+functionalPar <- fdPar(fdobj=basis, Lfdobj=2, lambda=1e3)
+
+# Smoothing the dataset
+for (i in 1:n) {
+  penalized = smooth.basis(t_ax, curves[i,], functionalPar)
+  p0 = eval.fd(t_ax, penalized$fd, Lfd=0)
+  pen_smooth0[i,] = t(p0)
+  p1 = eval.fd(t_ax, penalized$fd, Lfd=1)
+  pen_smooth1[i,] = t(p1)
+  p2 = eval.fd(t_ax, penalized$fd, Lfd=2)
+  pen_smooth2[i,] = t(p2)
+}
+
+#save(curves, pen_smooth0, pen_smooth1, pen_smooth2, file = "smooth_1600b_2D_1e3.RData")
+load("smooth_1600b_2D_1e3.RData")
+
+# compare original and smoothed data
+x11()
+par(mfrow=c(2,2))
+matplot(t_ax, t(curves), type = 'l', main = 'Original Data')
+matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data')
+matplot(t_ax, t(pen_smooth1), type = 'l', main = 'Original Data - First Derivative')
+matplot(t_ax, t(pen_smooth2), type = 'l', main = 'Smoothed Data - Second Derivative')
+
+x11()
+par(mfrow=c(1,2))
+matplot(t_ax, t(curves), type = 'l', main = 'Original Data')
+matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data')
+
+#Boh mi sembra uguale a quando penalizzo la derivata prima
+
+
+
+# Smoothing with 500 knots ------------------------------------------------
+
+# I don't use the points as knots. Instead, I decrease the number of knots to 500
+# smooth.basis command becomes faster
+
+range <- range(t_ax)
+nbasis_ <- 500
+basis <- create.bspline.basis(range, nbasis = nbasis_, norder=m)
+
+functionalPar <- fdPar(fdobj=basis, Lfdobj=1, lambda=1e3)
+
+# Smoothing the dataset --> jump down to load .RData
+for (i in 1:n) {
+  penalized = smooth.basis(t_ax, curves[i,], functionalPar)
+  p0 = eval.fd(t_ax, penalized$fd, Lfd=0)
+  pen_smooth0[i,] = t(p0)
+  p1 = eval.fd(t_ax, penalized$fd, Lfd=1)
+  pen_smooth1[i,] = t(p1)
+  p2 = eval.fd(t_ax, penalized$fd, Lfd=2)
+  pen_smooth2[i,] = t(p2)
+}
+
+#save(curves, pen_smooth0, pen_smooth1, pen_smooth2, file = "smooth_500b_1D_1e3.RData")
+load("smooth_500b_1D_1e3.RData")
+
+x11()
+par(mfrow=c(2,2))
+matplot(t_ax, t(curves), type = 'l', main = 'Original Data')
+matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data')
+matplot(t_ax, t(pen_smooth1), type = 'l', main = 'Smoothed Data - First Derivative')
+matplot(t_ax, t(pen_smooth2), type = 'l', main = 'Smoothed Data - Second Derivative')
+
+
+
+# Penalize 2nd derivative
+
+basis <- create.bspline.basis(range, nbasis = nbasis_, norder=m)
+
+functionalPar <- fdPar(fdobj=basis, Lfdobj=2, lambda=1e3)
+
+# Smoothing the dataset
+for (i in 1:n) {
+  penalized = smooth.basis(t_ax, curves[i,], functionalPar)
+  p0 = eval.fd(t_ax, penalized$fd, Lfd=0)
+  pen_smooth0[i,] = t(p0)
+  p1 = eval.fd(t_ax, penalized$fd, Lfd=1)
+  pen_smooth1[i,] = t(p1)
+  p2 = eval.fd(t_ax, penalized$fd, Lfd=2)
+  pen_smooth2[i,] = t(p2)
+}
+
+#save(curves, pen_smooth0, pen_smooth1, pen_smooth2, file = "smooth_500b_2D_1e3.RData")
+load("smooth_500b_2D_1e3.RData")
+
+x11()
+par(mfrow=c(2,2))
+matplot(t_ax, t(curves), type = 'l', main = 'Original Data')
+matplot(t_ax, t(pen_smooth0), type = 'l', main = 'Smoothed Data')
+matplot(t_ax, t(pen_smooth1), type = 'l', main = 'Smoothed Data - First Derivative')
+matplot(t_ax, t(pen_smooth2), type = 'l', main = 'Smoothed Data - Second Derivative') 
+
+
+
+# (FAILED) temptative of CV for the choice of lambda ----------------------
+
+
+# Cross validation for the choice of lambda
+# 
+# I do it on a single curve, the one with the steepest peak
+curve <- curves[which(curves[,500] > 700),]
+
+nbasis_ <- 300
+m <- 4 
+
+basis = create.bspline.basis(range, nbasis = nbasis_, norder=m)
+
+lambda=c(1,1e1,1e2,1e3,1e4,1e5)
+gcv=numeric(length(lambda))
+for (i in 1:length(lambda))
+{
+  functionalPar = fdPar(fdobj=basis, Lfdobj=1, lambda=lambda[i])  
+  gcv[i] = smooth.basis(t_ax, curve, functionalPar)$gcv
+}
+
+
+
+plot(log10(lambda),gcv)
+lambda[which.min(gcv)]
+
+# gcv mi dice che dovrei scegliere lambda piccolo, ma così non faccio undersmoothing?
+
+functionalPar = fdPar(fdobj=basis, Lfdobj=1, lambda=1e2)
+penalized <- smooth.basis(t_ax, curve, functionalPar)
+smoothed_curve = eval.fd(t_ax, penalized$fd, Lfd=0)
+
+x11()
+par(mfrow = c(1,2))
+plot(t_ax, curve, type="l", col = "red", main = "Original Curve")
+plot(t_ax, smoothed_curve, type="l", col = "blue", main = "Smoothe Curve")
+
+
 
