501cc0f8c84948e4cdd65c166af6bab56d3eba8c
tbortolotti <teresabortolotti16@gmail.com>
2021-02-13 12:22:25 +0100

    Grid mean dati veri

--- utility.R
+++ utility.R
@@ -19,12 +19,12 @@ X <- X[i,]
 fun <- function(PHI)
 {
   #val <- sum((-0.5)*log(2*pi*phi[j,]) - ((X[i,]-mu[j,])^2)/(2*phi[j,]) )
-  val <- sum((-0.5)*log(2*pi*PHI)*rep(1:100) - ((X-mu)^2)/(2*PHI) )
+  val <- sum((-0.5)*log(2*pi*PHI)*rep(1:30) - ((X-mu)^2)/(2*PHI) )
   return(val)
 }
 
 
-PHI <- seq(0.001,10,by=0.001)
+PHI <- seq(0.0001,10,by=0.0001)
 y <- numeric(length(PHI))
 for(i in 1:length(PHI))
 {
