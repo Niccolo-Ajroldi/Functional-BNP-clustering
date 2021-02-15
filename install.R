380c7abd92610b19787c77e880eb57274e83c919
Niccolò Ajroldi <n.ajroldi@gmail.com>
2021-02-15 18:08:41 +0100

    modified install.R

--- install.R
+++ install.R
@@ -1,25 +1,31 @@
 
-# automatic installation of required packages
-
+# required packages
 needed_packages <- c(
   "MASS",
   "fda",
-  "fdakma",
   "latex2exp",
   "roahd",
   "plyr",
   "coda",
   "devtools",
-  "mcclust.ext",
   "invgamma",
   "pbmcapply",
-  "LaplacesDemon"
+  "LaplacesDemon",
+  "devtools"
 )
 
+# new packages
 new_packages  <- needed_packages[!(needed_packages %in%installed.packages()[,"Package"])] 
 
+# install required packages
 if(length(new_packages))
 {
   install.packages(new_packages)
+}
+
+# install mcclust using devtools
+if(!c("mcclust.ext") %in% installed.packages())
+{
+  devtools::install_github("sarawade/mcclust.ext")
 }
 
